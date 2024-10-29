7f76f3a617714079b11461a629cfe33a442ecb54
philipcalderwood <philipcald@gmail.com>
2024-10-29 23:12:30 +0000

    WIP

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-class Clinician:
+class User:
+    '''
+    The global user profile through which all children are descended from. 
+
+    The user profile handles generic data such as email, password, first name, last name,
+    date of birth, address.
+    '''
+    def __init__(self) -> None:
+        pass
+    
     pass
