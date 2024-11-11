cd1f1daf7ecb52702503d0e01d9f781f53aec79f
philipcalderwood <philipcald@gmail.com>
2024-11-11 16:10:50 +0000

    Merging Ben's work on calendar

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 class User:
-    '''
-    The global user profile through which all children are descended from. 
+    """
+    The global user profile through which all children are descended from.
 
     The user profile handles generic data such as email, password, first name, last name,
     date of birth, address.
-    '''
-    def __init__(self) -> None:
-        pass
-    
+    """
+
+    def __init__(self, first_name, last_name, date_of_birth) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+
     pass
