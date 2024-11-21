e5fdae84e3d74d8e18c3e7f03e1f8b785edb9111
Ben Thompson <ben_thompson12@hotmail.com>
2024-11-21 22:26:43 +0000

    tested and fixed request_appointment method

--- modules/clinician.py
+++ modules/clinician.py
@@ -26,10 +26,12 @@ class Clinician(User):
         possible_hours = [9, 10, 11, 12, 14, 15, 16]
         available_slots = []
 
+        # This checks whether an appointment already exists at that day and time
+        # and that it isn't earlier than the current time
         for hour in possible_hours:
             if datetime(day.year, day.month, day.day, hour, 0) not in [
                 appointment["date"] for appointment in appointments
-            ]:
+            ] and datetime(day.year, day.month, day.day) > datetime.now():
                 available_slots.append(datetime(day.year, day.month, day.day, hour, 0))
 
         return available_slots
@@ -37,17 +39,24 @@ class Clinician(User):
     def request_appointment(self, patient: Patient) -> bool:
         """Allows the patient to request an appointment with their clinician"""
 
-        # Loop to take a requested date from the user
+        # Loop to take a valid requested date from the user
         def choose_date() -> datetime:
             while True:
                 try:
                     date_string = input(
-                        "Please enter a date when you would like to see your clinician (DD/MM/YY):"
+                        "Please enter a date when you would like to see your clinician (DD/MM/YY): "
                     )
 
-                    requested_date = datetime.strptime(date_string, "%d/%m/%y")
-                    if requested_date < datetime.today():
+                    requested_date = datetime.strptime(date_string.strip(), "%d/%m/%y")
+                    
+                    # Constructing a datetime for today's date minus the current time -
+                    # this allows patients to book slots on the day
+                    today = datetime.today()
+                    current_day = datetime(today.year, today.month, today.day)
+                    if requested_date < current_day:
                         print("You cannot book an appointment before the current date.")
+                    elif requested_date.weekday() in [5, 6]:
+                        print("Your clinician only works Monday-Friday. Please choose a date during the week.")
                     else:
                         return requested_date
                 except ValueError:
@@ -57,19 +66,25 @@ class Clinician(User):
 
         # Loop to select a valid time from the available slots
         def choose_slot(slots: list) -> datetime:
-            print("Here are the available times on that day:")
-            print(f"[{i + 1}] {slot.strftime('%H:%M')}" for i, slot in enumerate(slots))
+            print("\nHere are the available times on that day:")
+
+            for i, slot in enumerate(slots):
+                print(f"[{i + 1}] {slot.strftime('%H:%M')}")
+            print(f"[{len(slots) + 1}] Select a different day")
 
             while True:
                 selection = int(
                     input(
-                        f"Which time would you like to request? [{range(1, len(slots))}] "
+                        f"""\nWhich time would you like to request? 
+Please choose out of the following options: {[*range(1, len(slots) + 2)]} """
                     )
                 )
 
-                if selection not in list(range(1, len(slots))):
-                    print(f"Please select from options [{range(1, len(slots))}]")
+                if selection not in list(range(1, len(slots) + 2)):
+                    print("Invalid input. Please try again.")
                     continue
+                elif selection == len(slots) + 1:
+                    return False
                 else:
                     return slots[selection - 1]
 
@@ -87,7 +102,7 @@ class Clinician(User):
 
         # Take a description from the user - Phil can you advise on correct language here?
         description = input(
-            "Please describe why you would like to see your clinician (optional):"
+            "Please describe why you would like to see your clinician (optional): "
         )
 
         while True:
@@ -99,15 +114,18 @@ class Clinician(User):
 
             if not slots:
                 choose_again = input(
-                    "Sorry, your clinician has no availability on that day - would you like to choose another day? (Y/N)"
+                    "Sorry, your clinician has no availability on that day - would you like to choose another day? (Y/N) "
                 )
                 if choose_again == "N":
                     return False
                 else:
                     continue
-
+            
+            # Offer time slots to the user
             chosen_time = choose_slot(slots)
-            break
+
+            if chosen_time:
+                break
 
         try:
             self.database.cursor.execute(
@@ -123,7 +141,9 @@ class Clinician(User):
                 ),
             )
             self.database.connection.commit()
-            print("Your appointment has been requested. You'll receive an email once your clinician has confirmed it.")
+            print(
+                "\nYour appointment has been requested. You'll receive an email once your clinician has confirmed it."
+            )
             return True
         except sqlite3.IntegrityError as e:
             print(f"Failed to book appointment: {e}")
@@ -187,7 +207,7 @@ class Clinician(User):
             # Ben I have preserved number five as quit for now -> appreciate it
             # looks a little odd.
 
-            choices = ["Calendar", "Your Patient Dashboard", "-", "-", "Quit"]            
+            choices = ["Calendar", "Your Patient Dashboard", "-", "-", "Quit"]
             selection = display_choice("What would you like to do?", choices)
 
             if selection == 1:
