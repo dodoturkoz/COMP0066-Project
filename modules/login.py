b8a9197f204cccd8d476dc28299ef32e3375ae69
davidve0206 <davidve0206@gmail.com>
2024-12-03 14:37:43 +0000

    manage inactive user login

--- modules/login.py
+++ modules/login.py
@@ -52,9 +52,9 @@ def registration_input(
     existing_usernames: list[str], existing_emails: list[str]
 ) -> dict[str, Union[str, datetime]]:
     """
-    Gets the registration information and returns i
+    Gets the registration information and returns it
     """
-    # Collect user information in a dictionar
+    # Collect user information in a dictionary
     registration_info = {
         "username": "",
         "password": "",
@@ -119,9 +119,9 @@ def registration_input(
     display_dict(registration_info)
     register = get_valid_yes_or_no("Is this information correct? (Y/N): ")
     if register:
-        return registration_input(existing_usernames)
-    else:
         return registration_info
+    else:
+        return registration_input(existing_usernames, existing_emails)
 
 
 def signup(db: Database) -> bool:
@@ -131,6 +131,8 @@ def signup(db: Database) -> bool:
     Returns a boolean representing wether the signup was successful or not.
     If it is not successfull, prints a message to the user before quitting the app.
     """
+
+    clear_terminal()
 
     # Get a list of registered unique usernams
     existing_usernames = db.cursor.execute("SELECT username FROM Users").fetchall()
